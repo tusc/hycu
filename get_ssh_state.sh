9589f740dc8876fca2c6f2cc63dc3563a681e82a
tusc <carlos.talbot@gmail.com>
2022-05-21 16:47:11 -0500

    Create get_ssh_state.sh

---
+++ get_ssh_state.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+hycuctlr="192.168.1.80"
+username="admin"
+pass="newadmin"
+
+# request bearer TOKEN
+token=`curl -X POST -H "Accept: application/json" -sk -H "Authorization: Basic $(echo -n $username:$pass | base64)" "https://$hycuctlr:8443/rest/v1.0/requestToken" | jq -r '.token'`
+
+# convert TOKEN to base64
+btoken=$(echo -n $token | base64)
+
+# a return of FALSE denotes SSH lock is not enabled, TRUE denotes a lock
+curl -s -X GET --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $btoken" "https://$hycuctlr:8443/rest/v1.0/ssh/lock?pageSize=100" | jq -r ".entities[].sshLocked"
