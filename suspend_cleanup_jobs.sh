21322fd8f813750fba8a02092b7df3d512e091c1
tusc <carlos.talbot@gmail.com>
2022-05-21 18:10:08 -0500

    Create suspend_cleanup_jobs.sh

---
+++ suspend_cleanup_jobs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+hycuctlr="192.168.1.80"
+username="admin"
+pass="newadmin"
+
+# request bearer TOKEN
+token=`curl -X POST -H "Accept: application/json" -sk -H "Authorization: Basic $(echo -n $username:$pass | base64)" "https://$hycuctlr:8443/rest/v1.0/requestToken" | jq -r '.token'`
+
+# convert TOKEN to base64
+btoken=$(echo -n $token | base64)
+
+curl -s -X POST --insecure --header "Content-Type: application/json" --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $btoken" "https://$hycuctlr:8443/rest/v1.0/administration/scheduler/suspendCleanup" | jq
