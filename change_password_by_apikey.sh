88dc350e9879621d2a5087d3990b8866496bf144
tusc <carlos.talbot@gmail.com>
2022-05-22 10:34:00 -0500

    Update change_password_by_apikey.sh

--- change_password_by_apikey.sh
+++ change_password_by_apikey.sh
@@ -7,8 +7,7 @@ APIKey="bW12czh0ZjZjZjM5NnFjN2ZiNHRvdDgwaHZwaQ=="
 
 # user account you want to change password
 changeuser="testuser"
-origpass="newadmin123"
-newpass="newadmin456"
+newpass="newadmin"
 
 # Obtain the UUID for username
 uuid=`curl -s -X GET --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $APIKey" "https://$hycuctlr:8443/rest/v1.0/users?pageSize=100"  | jq ".entities[] | select (.username==\"$changeuser\")" | jq -r .uuid`
@@ -16,8 +15,8 @@ uuid=`curl -s -X GET --insecure --header "Accept: application/json" --insecure -
 # Obtain the user record by UUID
 user_record=`curl -s -X GET --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $APIKey" "https://$hycuctlr:8443/rest/v1.0/users/$uuid"  | jq ".entities[]"`
 
-# append the password fields to end of user record
-new_record=`echo $user_record |  jq '. += {"password":"'$newpass'", "oldpassword":"'$origpass'"}'`
+# append the password field to end of user record
+new_record=`echo $user_record |  jq '. += {"password":"'$newpass'"}'`
 
 # update user record with new password
 curl -s -X PATCH --insecure --header "Content-Type: application/json" --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $APIKey" "https://$hycuctlr:8443/rest/v1.0/users/$uuid" -d "$new_record" |jq
