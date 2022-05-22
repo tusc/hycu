efcc3382fdd2c99e5f551a3eb620346f641b2235
tusc <carlos.talbot@gmail.com>
2022-05-21 20:57:21 -0500

    Update list_users_by_apikey.sh

--- list_users_by_apikey.sh
+++ list_users_by_apikey.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
+#This script is an example where you do not need a username and password to authenticate. It uses an API key that you generate from the UI.
+#This script will not work unless you generate a key and save it in the script.
+
 hycuctlr="192.168.1.80"
 
-# you need to generate an API key from the UI in order to use this bearer token
 APIkey="bW12czh0ZjZjZjM5NnFjN2ZiNHRvdDgwaHZwaQ=="
 
 curl -s -X GET --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $APIkey" "https://$hycuctlr:8443/rest/v1.0/users?pageSize=100"  | jq
