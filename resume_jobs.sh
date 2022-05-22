118edf0e8218b415a3edb8f38e2b4baf1a8bb5c2
tusc <carlos.talbot@gmail.com>
2022-05-21 21:06:53 -0500

    Update resume_jobs.sh

--- resume_jobs.sh
+++ resume_jobs.sh
@@ -5,8 +5,7 @@ username="admin"
 pass="newadmin"
 
 # request bearer TOKEN
-token=`curl -X POST -H "Accept: application/json" -sk -H "Authorization: Basic $(echo -n $username:$pass | base64)" "https://$hycuctlr:8443/rest/v
-0/requestToken" | jq -r '.token'`
+token=`curl -X POST -H "Accept: application/json" -sk -H "Authorization: Basic $(echo -n $username:$pass | base64)" "https://$hycuctlr:8443/rest/v1.0/requestToken" | jq -r '.token'`
 
 # convert TOKEN to base64
 btoken=$(echo -n $token | base64)
