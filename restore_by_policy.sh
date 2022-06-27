aa5597c385ac76b62ba110bc7483195d98e94432
tusc <carlos.talbot@gmail.com>
2022-06-27 09:21:02 -0500

    Update restore_by_policy.sh

--- restore_by_policy.sh
+++ restore_by_policy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 # This script will run through all the VMs in a policy and perform a restore by cloning or overwriting the original VM.
 # The only variable that needs to be configured is RESTORE_MODE
-#
 
 hycuctlr="192.168.1.80"
 username="admin"
@@ -10,8 +9,8 @@ policy_name="Bronze"
 
 ################################
 # Set the following variable to either CLONE or OVERWRITE which determines how the VMs are restored
-RESTORE_MODE=CLONE
-#RESTORE_MODE=OVERWRITE
+#RESTORE_MODE=CLONE
+RESTORE_MODE=OVERWRITE
 #################################
 
 
@@ -46,13 +45,13 @@ do
 
         # Retrieve the most recent restore point
 
-        backup_info=`curl -s -X GET --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $btoken" "https://$hycuctlr:8443/rest/v1.0/vms/$vm_uuid/backups" | jq -r ".entities[0] | {uuid, type, primaryTargetName, hypervisorUuid, common} "`
+        backup_info=`curl -s -X GET --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $btoken" "https://$hycuctlr:8443/rest/v1.0/vms/$vm_uuid/backups?orderBy=-restorePointInMillis" | jq -r ".entities[0] | {uuid, type, primaryTargetName, hypervisorUuid, restorePointInMillis} "`
 
         backup_uuid=`echo $backup_info | jq -r ".uuid"`
         host_uuid=`echo $backup_info | jq -r ".hypervisorUuid"`
         backup_type=`echo $backup_info | jq -r ".type"`
         backup_target=`echo $backup_info | jq -r ".primaryTargetName"`
-        backup_created=`echo $backup_info | jq -r ".common.created" `
+        backup_created=`echo $backup_info | jq -r ".restorePointInMillis" `
         # times are in EPOCH milliseconds, need to convert to seconds
         backup_time=`date -d @$((($backup_created + 500)/1000))`
         echo -e "Using $backup_type\t\tFROM $backup_target\tusing restore point $backup_time"
@@ -79,15 +78,17 @@ do
                         net_type=`echo "$vnet" | jq -r ".virtualNetworkType"`
                         net_uuid=`echo "$vnet" | jq -r ".virtualNetworkId"`
 
-                        #build record for vnet list
+                        #build json array for vnet list
                         #the "'" is needed to ensure strings with spaces are correctly passed
                         vnet_list=`echo $vnet_list|  jq -r '. += [{"name":"'"$net_name"'", "uuid":"'"$net_uuid"'", "type":"'"$net_type"'", "macAddress":"'"$net_mac"'"}]'`
                 done
 
+                # final payload for restore REST request
                 restore_rec='{  "vmName": null, "vmUuid": "'$vm_uuid'", "hypervisorUuid": "'$host_uuid'", "backupUuid": "'$backup_uuid'", "containerId": null, "createVm": true, "deleteOriginalVm": true, "powerOn": false, "restoreSource": "AUTO", "createVolumeGroup": false, "attachVolumeGroup": true, "startVgRestore": false, "targetVmUuid": null, "restoreDisk": null, "virtualNetworkList": '$vnet_list'  }'
         fi
 
 #       # submit restore request
         rest_ret=`curl -s -X POST --insecure --header "Content-Type: application/json" --insecure --header "Accept: application/json" --insecure --header "Authorization: Bearer $btoken" "https://$hycuctlr:8443/rest/v1.0/vms/restore" -d "$restore_rec" | jq ".message.titleDescriptionEn"`
         echo "$rest_ret"
+#       exit 0
 done
