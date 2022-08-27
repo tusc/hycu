edaa8a0109b9a55a16bffaaab25082d66927c1d9
Carlos Talbot <carlos.talbot@gmail.com>
2022-08-27 18:08:09 -0500

    Update index_backups.py
    updated to include mounting of snapshot

--- search/index_backups.py
+++ search/index_backups.py
@@ -8,9 +8,13 @@
 #
 # You can use grub.cfg as a filename to search for within a Linux VM as the grub volume is searched early in the mount process
 # For Windows you can use desktop.ini
+# 2022/08/07 Initial release
+# 2022/08/27 Updated to include snapshot as source
 
 from http import client
-from sqlite3 import dbapi2
+#from sqlite3 import dbapi2
+from pickle import TRUE
+#from pickle import FALSE
 import sys
 import argparse
 import this
@@ -26,48 +30,32 @@ from pprint import pprint
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def huFindItemByValue(items, propName, propValue):
-    for key in items:
-        if items[key][propName] == propValue:
-            return items[key]
-    return None
-
 def huRestGeneric(url, timeout, pagesize, returnRaw=False, maxitems=None):
-    if pagesize > 0:
-        items = []
-        pageNumber = 1
-        while True:
-            requestUrl = "https://%s:8443/rest/v1.0/%spageSize=%d&pageNumber=%d" %(server, url, pagesize, pageNumber)
-            # make sure spaces, # and other special characters are encoded
-            parseURL = urllib.parse.quote(requestUrl, safe=":/&?=")
-            response = requests.get(parseURL,auth=(username,password), cert="",timeout=timeout,verify=False)
-            if response.status_code != 200:
-                print('Status:', response.status_code, 'Failed to retrieve REST results. Exiting.')
-                exit(response.status_code)
-
-            if returnRaw == True:
-                return response
-            
-            data = response.json()
-            items += data['entities']
-            pagesize = (data['metadata']['pageSize'])
-
-            # Exit the loop if we retrieved all of the items
-            if len(items) == (data['metadata']['totalEntityCount']):
-                break
-            if (maxitems != None) and maxitems < len(items):
-                break
-            pageNumber += 1
-    else:
-        requestUrl = "https://%s:8443/rest/v1.0/%s" %(server, url)
+    items = []
+    pageNumber = 1
+    while True:
+        requestUrl = "https://%s:8443/rest/v1.0/%spageSize=%d&pageNumber=%d" %(server, url, pagesize, pageNumber)
         # make sure spaces, # and other special characters are encoded
         parseURL = urllib.parse.quote(requestUrl, safe=":/&?=")
         response = requests.get(parseURL,auth=(username,password), cert="",timeout=timeout,verify=False)
         if response.status_code != 200:
             print('Status:', response.status_code, 'Failed to retrieve REST results. Exiting.')
             exit(response.status_code)
+
+        if returnRaw == True:
+            return response
+        
         data = response.json()
-        items = data['entities']
+        items += data['entities']
+        pagesize = (data['metadata']['pageSize'])
+
+        # Exit the loop if we retrieved all of the items
+        if len(items) == (data['metadata']['totalEntityCount']):
+            break
+        if (maxitems != None) and maxitems < len(items):
+            break
+        pageNumber += 1
+
     return items
 
 def huGetVMs(timeout, pagesize):
@@ -89,9 +77,9 @@ def huGetJobStatus(jobUuid, timeout):
 
     return data[0]['status']
 
-# retrive all backups for a given VM
-def huGetVMBackups(ntimeout, pageSize, vmuuid):
-    endpoint = "vms/" + vmuuid + "/backups?orderBy=-restorePointInMillis&"
+# retrive all backups or snapshots for a given VM
+def huGetVMImages(mounttype, ntimeout, pageSize, vmuuid):
+    endpoint = "vms/" + vmuuid + "/" + mounttype + "?"
  
     data = huRestGeneric(endpoint, ntimeout, pageSize)
     return data
@@ -120,19 +108,43 @@ def hUpdateVMs(ntimeout, pageSize):
     for item in data:
         result=hycudb.vms.insert_one(item)          
 
-# check to see if VM mount is active
-def huCheckMount(ntimeout, pageSize, vmuuid, backup_uuid):
-    endpoint = "vms/" + vmuuid + "/backups/" + backup_uuid + "/mount?restoreSource=AUTO&"
+def huExpireOldFiles():
+    hycucol_backups = hycudb["backups"]
+    hycucol_files = hycudb["files"]
 
+    # return all active unique backup UUIDs from the backups table
+    backup_uuids = hycucol_backups.distinct('uuid')
+
+    print("before uuid remove")
+    col_count=hycucol_files.count_documents( {'backupUuid': {'$nin': backup_uuids}} )
+    #if 
+    #print ())
+    print("after uuid remove")    
+    # backup_uuids.remove("05fc727b-3277-4efe-bc10-a3e3d323a289")
+    print (hycucol_files.count_documents( {'backupUuid': {'$nin': backup_uuids}} ))
+
+    rec_count=hycucol_files.count_documents( {'backupUuid': {'$nin': backup_uuids}})
+    print(f'removing {rec_count} records')
+
+    # Remove all file records that belonged to an EXPIRED backup restore point
+    # (i.e. backup UUID no longer in backups table)
+    # We use the Python keyword $nin below to denote NOT IN. So return all file records with
+    # a backup UUID NOT IN the Python list from above.
+#    hycucol_files.remove( {'backupUuid': {'$nin': backup_uuids}})
+
+# check to see if VM mount is active
+def huCheckMount(mounttype,ntimeout, pageSize, vmuuid, backup_uuid):
+# mountype can be backups or snapshots
+    endpoint = "vms/" + vmuuid + "/" + mounttype + "/" + backup_uuid + "/mount?restoreSource=AUTO&"
     data = huRestGeneric(endpoint, ntimeout, pageSize)
     return data
 
-
-# mount VM backup, wait for job to complete
-def huMountBackup(timeout, vmuuid, backup_uuid):
+# mount VM backup/snapshot, wait for job to complete
+def huMount(mounttype, timeout, vmuuid, backup_uuid):
     options = '{ "restoreSource": "AUTO"}'    
 
-    requestUrl = "https://%s:8443/rest/v1.0/vms/%s/backups/%s/mount" %(server, vmuuid, backup_uuid)
+    requestUrl = "https://%s:8443/rest/v1.0/vms/%s/%s/%s/mount" %(server, vmuuid, mounttype, backup_uuid)
+
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json, text/plain, */*'
@@ -160,11 +172,12 @@ def huMountBackup(timeout, vmuuid, backup_uuid):
 
     return result
 
-# unmount VM backup, wait for job to complete
-def huUnmountBackup(timeout, vmuuid, backup_uuid):
+# unmount VM backup/snapshot, wait for job to complete
+def huUnmount(mounttype, timeout, vmuuid, backup_uuid):
     #options = '{ "restoreSource": "AUTO"}'    
 
-    requestUrl = "https://%s:8443/rest/v1.0/vms/%s/backups/%s/mount" %(server, vmuuid, backup_uuid)
+    requestUrl = "https://%s:8443/rest/v1.0/vms/%s/%s/%s/mount" %(server, vmuuid, mounttype, backup_uuid)
+
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json, text/plain, */*'
@@ -191,19 +204,6 @@ def huUnmountBackup(timeout, vmuuid, backup_uuid):
         exit(1)
 
     return result
-
-# Retrieve the list of VMs from HYCU Rest API server (page by page)
-def huFindVM(vmname, ntimeout, pageSize):
-    vms = huGetVMs(ntimeout, pageSize)
-
-    # Check if the given VM name is valid
-    vm = huFindItemByValue(vms, 'vmName', vmname)
-    if (vm == None):
-        print('Status:', 'Cannot find VM "%s" in the list of HYCU VMs' %(vmname))
-        exit(1)
-
-    print ("VM " + vm['vmName'] + " found. VM UUID is " + vm['uuid'])
-    return vm
 
 def huBrowseMount(mountpath):
 #    endpoint = "mounts/" + mount_uuid + "/browse?filter=subType==2&orderBy=displayName&path=" + mountpath + "&"
@@ -253,9 +253,7 @@ def main(argv):
     myParser.add_argument("-p", "--password", help="HYCU Password", required=True)
     myParser.add_argument("-v", "--vm", help="VM to be searched up", required=True)    
     myParser.add_argument("-m", "--mongodb", help="Connection string to Mongodb", required=True)
-    myParser.add_argument("-sf", "--statusfilter", choices=['EXECUTING', 'OK', 'WARNING', 'ERROR', 'FATAL', 'QUEUED', 'ABORTED'], help="Filter jobs", required=False)
-    myParser.add_argument("-i", "--timeout", help="Advanced: REST Query timeout [default=5]", required=False)
-    myParser.add_argument("-z", "--pagesize", help="Advanced: REST Query pagesize [default=None]", required=False)
+    myParser.add_argument("-r", "--refresh", help="Advanced: refresh MongoDB [Default=False]", required=False)    
 
     if len(sys.argv)==1:
         myParser.print_help()
@@ -267,58 +265,97 @@ def main(argv):
     server=args.server
   
     # REST call intializations
-    nTimeout = 5 if not args.timeout else int(args.timeout)
-    pageSize = 50 if not args.pagesize else int(args.pagesize)
+    nTimeout = 60
+    pageSize = 50
+    refresh_db=None if not args.refresh else (args.refresh)
 
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+    # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
     client = MongoClient(args.mongodb)
     hycudb=client.hycu
 
-    #refersh VM backups list in MongoDB with latest
-    hUpdateVMBackups(nTimeout, pageSize)
-    #refersh VM list in MongoDB with latest
-    hUpdateVMs(nTimeout, pageSize)    
+    if refresh_db:
+        #refersh VM backups list in MongoDB with latest
+        print("Please wait updating VM Backup list...")
+        hUpdateVMBackups(nTimeout, pageSize)
+        print("Please wait updating VM list...")
+        #refersh VM list in MongoDB with latest
+        hUpdateVMs(nTimeout, pageSize)    
+
+        # expire old file entries in database
+#        huExpireOldFiles()
 
     start_time = datetime.datetime.now()
     print("Current Time =", start_time)
 
-    vm = huFindVM(args.vm, nTimeout, pageSize)
+    # find VM
+    endpoint = "vms?filter=vmName##" + args.vm + "&"    
+    vm=huRestGeneric(endpoint, timeout=5, pagesize=50, returnRaw=False, maxitems=None)
+    if not vm:
+        # can't find VM, exit
+        print ("Can't find VM " + vm_name)            
+        exit(1)
 
-    # retrieve all backups for VM
-    vmbackups = huGetVMBackups(nTimeout, pageSize, vm['uuid'])
-    # save this UUID to store with files in DB
-    vm_uuid=vm['uuid']
-    
+    vm_uuid=vm[0]['uuid']
+    vm_type = vm[0]['externalHypervisorType']
+
+    use_snap = None
+    # Check to see if VM is on Nutanix, if so then try to retrive snapshot and use for mount.
+    # Otherwise fall back to backup as source of mount.
+    if (vm_type == "KVM" or vm_type == "VMware"):
+        vmbackups = huGetVMImages("snapshots",nTimeout, pageSize, vm_uuid)
+        if not vmbackups:
+            # no snapshots available, use backup image as mount source
+            vmbackups = huGetVMImages("backups", pageSize, vm_uuid)
+        else:
+            # we found a snapshot, will use as mount source
+            use_snap = True
+    else:
+        # vSphere backup, not snapshot available 
+        vmbackups = huGetVMImages("backups",nTimeout, pageSize, vm_uuid)
+
     if not vmbackups:
         print ("No backups found!")
         exit (1)
-    # save this UUID to store with files in DB
+
+    if use_snap:
+        mount_type="snapshots"
+    else:
+        mount_type="backups"
+        
+    # save this backup UUID to store with files in DB
     backup_uuid=vmbackups[0]['uuid']
-    # get backup time from most recent backup
-    backup_time = ((vmbackups[0]['restorePointInMillis']+500)/1000)
-    backup_str = datetime.datetime.fromtimestamp(backup_time).strftime('%c')
-    print ("Searching " + vmbackups[0]['type'] + " from " + backup_str + " on " + vmbackups[0]['primaryTargetName'])
+
+    if (use_snap):
+        # get backup time from most recent backup
+        #backup_time = ((vmbackups[0]['restorePointInMillis']+500)/1000)
+        #backup_str = datetime.datetime.fromtimestamp(backup_time).strftime('%c')
+        print ("Searching snapshot " + backup_uuid)
+    else:
+        # get backup time from most recent backup
+        backup_time = ((vmbackups[0]['restorePointInMillis']+500)/1000)
+        backup_str = datetime.datetime.fromtimestamp(backup_time).strftime('%c')
+        print ("Searching " + vmbackups[0]['type'] + " from " + backup_str + " on " + vmbackups[0]['primaryTargetName'])
 
     # check if VM has any mounts currently
-    mount_state = huCheckMount(nTimeout, pageSize, vm['uuid'], vmbackups[0]['uuid'])
+    mount_state = huCheckMount(mount_type,nTimeout, pageSize, vm[0]['uuid'], vmbackups[0]['uuid'])
     if not mount_state[0]['mounted']:
-        print (vm['vmName'] + " is not MOUNTED...mounting..")
-        mount_data = huMountBackup(nTimeout, vm['uuid'], vmbackups[0]['uuid'])
+        print (vm[0]['vmName'] + " is not MOUNTED...mounting..")
+        mount_data = huMount(mount_type,nTimeout, vm[0]['uuid'], vmbackups[0]['uuid'])
         if not mount_data['entities'][0]:
             print ("Mount error!")
             exit(1)
         # grab mount id from checkmount
-        mount_state = huCheckMount(nTimeout, pageSize, vm['uuid'], vmbackups[0]['uuid'])
+        mount_state = huCheckMount(mount_type,nTimeout, pageSize, vm[0]['uuid'], vmbackups[0]['uuid'])
         mount_uuid=mount_state[0]['mountUuid']        
     else:
-        print (vm['vmName'] + " is MOUNTED, resuing MountID....")
+        print (vm[0]['vmName'] + " is MOUNTED, resuing MountID....")
         mount_uuid=mount_state[0]['mountUuid']
 
     # Time to walk the tree!
     results = huBrowseMount("")
 
     #unmount backup before exiting
-    mount_data = huUnmountBackup(nTimeout, vm['uuid'], vmbackups[0]['uuid'])
+    mount_data = huUnmount(mount_type,nTimeout, vm[0]['uuid'], vmbackups[0]['uuid'])
 
     end_time = datetime.datetime.now()
     print("Current Time =", end_time)
